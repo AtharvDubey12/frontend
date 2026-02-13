99d0fbfe4040b283205f1169344f6c3fc0942575
Atharv Dubey <athdubey120904@gmail.com>
2026-02-13 13:27:05 +0530

    updated worker script

--- public/velvex.worker.js
+++ public/velvex.worker.js
@@ -1,33 +1,4 @@
-// /* eslint-disable no-restricted-globals */
-// importScripts('/velvex_compiler.js'); 
-
-// let compilerModule = null;
-
-// self.onmessage = async (e) => {
-//   const { code } = e.data;
-
-//   if (!compilerModule) {
-//     // createVelvexModule is defined in the generated glue code
-//     compilerModule = await self.createVelvexModule({
-//       print: (text) => {
-//         self.postMessage({ type: 'stdout', text: text });
-//       },
-//       printErr: (text) => {
-//         self.postMessage({ type: 'stderr', text: text });
-//       }
-//     });
-//   }
-
-//   // Clear previous output state if necessary and call the C++ function
-//   compilerModule.ccall('compile_velvet', null, ['string'], [code]);
-//   self.postMessage({ type: 'done' });
-// };
-
-/* eslint-disable no-restricted-globals */
-
-// Load the Emscripten glue code (served from /public)
 importScripts("/velvex_compiler.js");
-  console.log("WORKER CREATED");
 let compilerModule = null;
 let initializing = false;
 
